fbd0d48ff9b607bca55abb8dda5a99828e09026f
Caique <caiquenaimi.564@gmail.com>
2023-08-14 11:18:30 -0300

    feat :  remove, and add

--- assets/js/script.index.js
+++ assets/js/script.index.js
@@ -1,24 +1,35 @@
-/* function executarMetodo () {
-    const instancia = new Minhaclasse();
-    instancia.meuMetodo();
-} */
-
+let verified = false;
+let i = 0;
 
 class Task {
-    constructor(task){
+    constructor(task) {
         this.task = task;
-    } 
+    }
 
     showTask(task) {
         document.getElementById('results').innerHTML += `<div class="task"><p>${task}</p>
-        <button id="verify"><img src = "./assets/images/check-svgrepo-com.svg"></button>
-        
-        
-        </div>`
-    }   
+        <button onclick= "verifyTask()" id="verify"><img src = "./assets/images/check-svgrepo-com (1).svg"></button>
+        <button onclick = "removeTask()" id = "trash" ><img  src = "./assets/images/trash-blank-alt-svgrepo-com.svg"></button>
+        </div>`;
+    }
 }
 
-function addTask () {
+function addTask() {
     const task = new Task();
     task.showTask(text.value);
+}
+
+
+
+function verifyTask() {
+    if (verified == false) {
+        document.getElementById("verify").style.backgroundColor = 'green';
+        verified = true;
+    } else {
+        document.getElementById("verify").style.backgroundColor = '#3f3f3f';
+        verified = true;
+    }
+}
+function removeTask() {
+    document.getElementById('results').remove();
 }
